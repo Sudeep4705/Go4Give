7de56dc792c3518246661e6c5edbcb5d299576f4
Sudeep <sudeepsudee828@gmail.com>
2025-08-14 14:57:29 +0530

    cookie

--- frontend/src/landing_Page/Navbar.jsx
+++ frontend/src/landing_Page/Navbar.jsx
@@ -1,110 +1,25 @@
-// import './Navbar.css'
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
-
-// import axios from "axios";
-//   function Navbar(){
-//   const [isLoggedIn, setIsLoggedIn] = useState(false); 
-//   const navigate = useNavigate();
-//     useEffect(() => {
-//  axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setIsLoggedIn(data.loggedIn);
-//     })
-//     .catch((err) => {
-//       console.error("Auth check failed", err);
-//       setIsLoggedIn(false);
-//     });
-// }, []);
-
-// const handleLogout = async () => {
-//   try {
-//     // await fetch("http://localhost:8000/user/logout", {
-//     //   method: "POST",
-//     //   credentials: "include",
-//     // });
-//     await axios.post(
-//   `${import.meta.env.VITE_API_URL}/user/logout`,
-//   { withCredentials: true }
-// );
-
-//     setIsLoggedIn(false);
-//     navigate("/login");
-//   } catch (err) {
-//     console.error("Logout failed", err);
-//   }
-// };
-//     return(
-//         <>
-//         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//   <div className="container-fluid">
-//     <a className="navbar-brand" href="#"><img className='img' src="images/logo1.svg" alt="" /></a>
-//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
-//     </button>
-//     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//       <ul className="navbar-nav">
-//         <li className="nav-item">
-//           <a className="nav-link active"  href="/">Home</a>
-//         </li>
-//         <li className="nav-item"> 
-//           <a className="nav-link active" href="/about">About</a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link active" href="/contact">Contact</a>
-//         </li>
-//         <li className="nav-item dropdown">
-//           <a className="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//             Explore
-//           </a>
-//           <ul className="dropdown-menu">
-//             <li><a className="dropdown-item" href="/fundraiser">Fundraiser</a></li>
-//             <li><a className="dropdown-item" href="/blog">Blog</a></li>
-            
-//           </ul>
-//         </li>
-//         {/*  ADD THIS INSIDE THE <ul> */}
-//   {isLoggedIn && (
-//     <li className="nav-item">
-//       <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
-//         Logout
-//       </button>
-//     </li>
-//   )}
-
-//       </ul>
-      
-//     </div>
-//   </div>
-// </nav>
-
-//         </>
-//     )
-//   }
-
-//   export default Navbar;
-
-
-import './Navbar.css';
 import { useEffect, useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
+  // Check login status on mount
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
-      .then((res) => {
+    const checkLogin = async () => {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, {
+          withCredentials: true,
+        });
         setIsLoggedIn(res.data.loggedIn);
-      })
-      .catch((err) => {
-        console.error("Auth check failed", err);
+      } catch (err) {
         setIsLoggedIn(false);
-      });
+      }
+    };
+
+    checkLogin();
   }, []);
 
   const handleLogout = async () => {
@@ -114,7 +29,6 @@ function Navbar() {
         {},
         { withCredentials: true }
       );
-
       setIsLoggedIn(false);
       navigate("/login");
     } catch (err) {
@@ -126,10 +40,18 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          <img className='img' src="images/logo1.svg" alt="Logo" />
+          <img className="img" src="images/logo1.svg" alt="Logo" />
         </Link>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
@@ -162,7 +84,10 @@ function Navbar() {
 
             {isLoggedIn && (
               <li className="nav-item">
-                <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
+                <button
+                  className="btn btn-outline-danger ms-2"
+                  onClick={handleLogout}
+                >
                   Logout
                 </button>
               </li>
@@ -175,4 +100,3 @@ function Navbar() {
 }
 
 export default Navbar;
-
