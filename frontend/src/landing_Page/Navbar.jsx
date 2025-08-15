6c9dcf284ad29849a6a6d74011e16c4ad003087e
Sudeep <sudeepsudee828@gmail.com>
2025-08-15 13:08:43 +0530

    cookie

--- frontend/src/landing_Page/Navbar.jsx
+++ frontend/src/landing_Page/Navbar.jsx
@@ -1,126 +1,189 @@
-// import './Navbar.css'
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { Link } from "react-router-dom";
+// // import './Navbar.css'
+// // import { useEffect, useState } from "react";
+// // import { useNavigate } from "react-router-dom";
+// // import { Link } from "react-router-dom";
 
-// import axios from "axios";
-//   function Navbar(){
-//   const [isLoggedIn, setIsLoggedIn] = useState(false); 
-//   const navigate = useNavigate();
-//     useEffect(() => {
-//  axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       setIsLoggedIn(data.loggedIn);
-//     })
-//     .catch((err) => {
-//       console.error("Auth check failed", err);
-//       setIsLoggedIn(false);
-//     });
-// }, []);
+// // import axios from "axios";
+// //   function Navbar(){
+// //   const [isLoggedIn, setIsLoggedIn] = useState(false); 
+// //   const navigate = useNavigate();
+// //     useEffect(() => {
+// //  axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
+// //     .then((res) => res.json())
+// //     .then((data) => {
+// //       setIsLoggedIn(data.loggedIn);
+// //     })
+// //     .catch((err) => {
+// //       console.error("Auth check failed", err);
+// //       setIsLoggedIn(false);
+// //     });
+// // }, []);
 
-// const handleLogout = async () => {
-//   try {
-//     // await fetch("http://localhost:8000/user/logout", {
-//     //   method: "POST",
-//     //   credentials: "include",
-//     // });
-//     await axios.post(
-//   `${import.meta.env.VITE_API_URL}/user/logout`,
-//   { withCredentials: true }
-// );
+// // const handleLogout = async () => {
+// //   try {
+// //     // await fetch("http://localhost:8000/user/logout", {
+// //     //   method: "POST",
+// //     //   credentials: "include",
+// //     // });
+// //     await axios.post(
+// //   `${import.meta.env.VITE_API_URL}/user/logout`,
+// //   { withCredentials: true }
+// // );
 
-//     setIsLoggedIn(false);
-//     navigate("/login");
-//   } catch (err) {
-//     console.error("Logout failed", err);
-//   }
-// };
-//     return(
-//         <>
-//         <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//   <div className="container-fluid">
-//     <a className="navbar-brand" href="#"><img className='img' src="images/logo1.svg" alt="" /></a>
-//     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-//       <span className="navbar-toggler-icon"></span>
-//     </button>
-//     <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//       <ul className="navbar-nav">
-//         <li className="nav-item">
-//           <a className="nav-link active"  href="/">Home</a>
-//         </li>
-//         <li className="nav-item"> 
-//           <a className="nav-link active" href="/about">About</a>
-//         </li>
-//         <li className="nav-item">
-//           <a className="nav-link active" href="/contact">Contact</a>
-//         </li>
-//         <li className="nav-item dropdown">
-//           <a className="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//             Explore
-//           </a>
-//           <ul className="dropdown-menu">
-//             <li><a className="dropdown-item" href="/fundraiser">Fundraiser</a></li>
-//             <li><a className="dropdown-item" href="/blog">Blog</a></li>
+// //     setIsLoggedIn(false);
+// //     navigate("/login");
+// //   } catch (err) {
+// //     console.error("Logout failed", err);
+// //   }
+// // };
+// //     return(
+// //         <>
+// //         <nav className="navbar navbar-expand-lg bg-body-tertiary">
+// //   <div className="container-fluid">
+// //     <a className="navbar-brand" href="#"><img className='img' src="images/logo1.svg" alt="" /></a>
+// //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+// //       <span className="navbar-toggler-icon"></span>
+// //     </button>
+// //     <div className="collapse navbar-collapse" id="navbarNavDropdown">
+// //       <ul className="navbar-nav">
+// //         <li className="nav-item">
+// //           <a className="nav-link active"  href="/">Home</a>
+// //         </li>
+// //         <li className="nav-item"> 
+// //           <a className="nav-link active" href="/about">About</a>
+// //         </li>
+// //         <li className="nav-item">
+// //           <a className="nav-link active" href="/contact">Contact</a>
+// //         </li>
+// //         <li className="nav-item dropdown">
+// //           <a className="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+// //             Explore
+// //           </a>
+// //           <ul className="dropdown-menu">
+// //             <li><a className="dropdown-item" href="/fundraiser">Fundraiser</a></li>
+// //             <li><a className="dropdown-item" href="/blog">Blog</a></li>
             
-//           </ul>
-//         </li>
-//         {/*  ADD THIS INSIDE THE <ul> */}
-//   {isLoggedIn && (
-//     <li className="nav-item">
-//       <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
-//         Logout
-//       </button>
-//     </li>
-//   )}
+// //           </ul>
+// //         </li>
+// //         {/*  ADD THIS INSIDE THE <ul> */}
+// //   {isLoggedIn && (
+// //     <li className="nav-item">
+// //       <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
+// //         Logout
+// //       </button>
+// //     </li>
+// //   )}
 
-//       </ul>
+// //       </ul>
       
-//     </div>
-//   </div>
-// </nav>
+// //     </div>
+// //   </div>
+// // </nav>
 
-//         </>
-//     )
-//   }
+// //         </>
+// //     )
+// //   }
 
-//   export default Navbar;
+// //   export default Navbar;
+
+
+// import './Navbar.css';
+// import { useEffect, useState } from "react";
+// import { useNavigate, Link } from "react-router-dom";
+// import axios from "axios";
+
+// function Navbar() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
+//       .then((res) => {
+//         setIsLoggedIn(res.data.loggedIn);
+//       })
+//       .catch((err) => {
+//         console.error("Auth check failed", err);
+//         setIsLoggedIn(false);
+//       });
+//   }, []);
+
+//   const handleLogout = async () => {
+//     try {
+//       await axios.post(
+//         `${import.meta.env.VITE_API_URL}/user/logout`,
+//         {},
+//         { withCredentials: true }
+//       );
+
+//       setIsLoggedIn(false);
+//       navigate("/login");
+//     } catch (err) {
+//       console.error("Logout failed", err);
+//     }
+//   };
+
+//   return (
+//     <nav className="navbar navbar-expand-lg bg-body-tertiary">
+//       <div className="container-fluid">
+//         <Link className="navbar-brand" to="/">
+//           <img className='img' src="images/logo1.svg" alt="Logo" />
+//         </Link>
+
+//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div className="collapse navbar-collapse" id="navbarNavDropdown">
+//           <ul className="navbar-nav">
+//             <li className="nav-item">
+//               <Link className="nav-link active" to="/">Home</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link active" to="/about">About</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link active" to="/contact">Contact</Link>
+//             </li>
+//             <li className="nav-item dropdown">
+//               <Link
+//                 className="nav-link dropdown-toggle active"
+//                 to="#"
+//                 role="button"
+//                 data-bs-toggle="dropdown"
+//                 aria-expanded="false"
+//               >
+//                 Explore
+//               </Link>
+//               <ul className="dropdown-menu">
+//                 <li><Link className="dropdown-item" to="/fundraiser">Fundraiser</Link></li>
+//                 <li><Link className="dropdown-item" to="/blog">Blog</Link></li>
+//               </ul>
+//             </li>
+
+//             {isLoggedIn && (
+//               <li className="nav-item">
+//                 <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
+//                   Logout
+//                 </button>
+//               </li>
+//             )}
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
 
 
 import './Navbar.css';
-import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import axios from "axios";
+import { UserButton, useUser } from "@clerk/clerk-react";
 
 function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const { isSignedIn } = useUser();
   const navigate = useNavigate();
-
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/user/verify`, { withCredentials: true })
-      .then((res) => {
-        setIsLoggedIn(res.data.loggedIn);
-      })
-      .catch((err) => {
-        console.error("Auth check failed", err);
-        setIsLoggedIn(false);
-      });
-  }, []);
-
-  const handleLogout = async () => {
-    try {
-      await axios.post(
-        `${import.meta.env.VITE_API_URL}/user/logout`,
-        {},
-        { withCredentials: true }
-      );
-
-      setIsLoggedIn(false);
-      navigate("/login");
-    } catch (err) {
-      console.error("Logout failed", err);
-    }
-  };
 
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -129,7 +192,15 @@ function Navbar() {
           <img className='img' src="images/logo1.svg" alt="Logo" />
         </Link>
 
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
 
@@ -160,11 +231,10 @@ function Navbar() {
               </ul>
             </li>
 
-            {isLoggedIn && (
-              <li className="nav-item">
-                <button className="btn btn-outline-danger ms-2" onClick={handleLogout}>
-                  Logout
-                </button>
+            {isSignedIn && (
+              <li className="nav-item ms-3">
+                {/* Clerk's built-in user menu & logout button */}
+                <UserButton afterSignOutUrl="/login" />
               </li>
             )}
           </ul>
@@ -175,4 +245,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
 
--- frontend/src/landing_Page/User/Login.jsx
+++ frontend/src/landing_Page/User/Login.jsx
@@ -149,87 +149,154 @@
 
 // export default Login;
 
-import React from "react";
-import { ClerkProvider, SignIn } from "@clerk/clerk-react";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Clerk Publishable Key in .env");
-}
+import React, { useState } from "react";
+import { useSignIn } from "@clerk/clerk-react";
 
 export default function Login() {
+  const { signIn, setActive, isLoaded } = useSignIn();
+  const [login, setLogin] = useState({ username: "", password: "" });
+  const [message, setMessage] = useState("");
+
+  const handleChange = (e) => {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!isLoaded) return;
+
+    try {
+      const result = await signIn.create({
+        identifier: login.username,
+        password: login.password,
+      });
+
+      if (result.status === "complete") {
+        await setActive({ session: result.createdSessionId });
+        setMessage("Login successful!");
+      } else {
+        setMessage("Login needs more steps.");
+      }
+    } catch (err) {
+      setMessage(err.errors ? err.errors[0].message : "Login failed");
+    }
+  };
+
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <div
+      style={{
+        position: "relative",
+        backgroundImage: `url("/images/signup.jpg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "100vh",
+        color: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "20px",
+      }}
+    >
+      {/* Overlay */}
       <div
         style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          zIndex: 1,
+        }}
+      />
+
+      {/* Form Card */}
+      <div
+        className="shadow"
+        style={{
           position: "relative",
-          backgroundImage: `url("/images/signup.jpg")`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "100vh",
+          zIndex: 2,
+          backgroundColor: "rgba(0, 0, 0, 0.6)",
+          backdropFilter: "blur(6px)",
+          borderRadius: "20px",
+          padding: "35px 30px",
+          width: "100%",
+          maxWidth: "420px",
+          boxShadow: "0 15px 35px rgba(0,0,0,0.3)",
           color: "#fff",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px",
         }}
       >
-        {/* Overlay */}
-        <div
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-            zIndex: 1,
-          }}
-        />
+        <h2 className="text-center mb-4" style={{ fontWeight: "600" }}>
+          Login to your account
+        </h2>
+        <h4 className="text-center" style={{ color: "#f88" }}>
+          {message}
+        </h4>
 
-        {/* Card */}
-        <div
-          className="shadow"
-          style={{
-            position: "relative",
-            zIndex: 2,
-            backgroundColor: "rgba(0, 0, 0, 0.6)",
-            backdropFilter: "blur(6px)",
-            borderRadius: "20px",
-            padding: "35px 30px",
-            width: "100%",
-            maxWidth: "420px",
-            boxShadow: "0 15px 35px rgba(0,0,0,0.3)",
-            color: "#fff",
-          }}
-        >
-          <h2
-            className="text-center mb-4"
-            style={{ fontWeight: "600", color: "#fff" }}
+        <form onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label
+              htmlFor="username"
+              className="form-label"
+              style={{ fontSize: "15px" }}
+            >
+              Username
+            </label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={login.username}
+              onChange={handleChange}
+              className="form-control rounded-3"
+              style={{
+                padding: "10px",
+                fontSize: "14px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                border: "1px solid rgba(255,255,255,0.3)",
+                color: "#fff",
+              }}
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="form-label"
+              style={{ fontSize: "15px" }}
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={login.password}
+              onChange={handleChange}
+              className="form-control rounded-3"
+              style={{
+                padding: "10px",
+                fontSize: "14px",
+                backgroundColor: "rgba(255,255,255,0.1)",
+                border: "1px solid rgba(255,255,255,0.3)",
+                color: "#fff",
+              }}
+              required
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn btn-light w-100 rounded-pill py-2"
+            style={{ fontWeight: "500" }}
           >
-            Login to your account
-          </h2>
-
-          {/* Clerk Sign In */}
-          <SignIn
-            appearance={{
-              elements: {
-                formButtonPrimary:
-                  "bg-white text-black font-semibold py-2 px-4 rounded-pill",
-                formFieldInput:
-                  "bg-transparent border border-gray-300 text-white rounded-3 px-3 py-2",
-                formFieldLabel: "text-white",
-              },
-            }}
-            routing="path"
-            path="/login"
-            signUpUrl="/signup"
-            afterSignInUrl="/"
-          />
-        </div>
+            Login
+          </button>
+        </form>
       </div>
-    </ClerkProvider>
+    </div>
   );
 }
+
 
